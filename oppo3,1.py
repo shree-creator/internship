e0de22d211e39738bbac6df453b0b8a24f1723ef
shree-creator <4mu22cs027@mysururoyal.org>
2026-02-05 09:11:05 +0530

    Add files via upload

---
+++ oppo3,1.py
@@ -0,0 +1,49 @@
+class Product:
+    def __init__(self, product_name, price):
+        self.product_name = product_name
+        self.price = price
+
+    def display_product(self):
+        print("Product Name:", self.product_name)
+        print("Price:", self.price)
+
+
+
+class ElectronicProduct(Product):
+    def __init__(self, product_name, price, brand, warranty):
+        super().__init__(product_name, price)
+        self.brand = brand
+        self.warranty = warranty
+
+    def display_electronic_product(self):
+        print("Brand:", self.brand)
+        print("Warranty:", self.warranty, "years")
+
+
+
+class MobilePhone(ElectronicProduct):
+    def __init__(self, product_name, price, brand, warranty, ram, storage):
+        super().__init__(product_name, price, brand, warranty)
+        self.ram = ram
+        self.storage = storage
+
+    def display_mobile_details(self):
+        print("RAM:", self.ram)
+        print("Storage:", self.storage)
+
+
+
+mobile = MobilePhone(
+    "Smartphone",
+    25000,
+    "Samsung",
+    2,
+    "8 GB",
+    "128 GB"
+)
+
+
+print("---- Mobile Phone Details ----")
+mobile.display_product()
+mobile.display_electronic_product()
+mobile.display_mobile_details()
